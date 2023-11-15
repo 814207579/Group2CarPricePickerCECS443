a02f7d1dc04c2770f6643d2e2778d0c89ab8d286
samnanglath01 <112363663+samnanglath01@users.noreply.github.com>
2023-11-14 19:29:55 -0800

    implemented search

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,19 +1,35 @@
-
 document.addEventListener('DOMContentLoaded', function() {
+    let allCars = [];
+
+    // Fetch and display cars
     fetch('https://fakestoreapi.com/products')
     .then(response => response.json())
     .then(data => {
-        const carListings = document.getElementById('car-listings');
-        data.forEach(car => {
-            const carDiv = document.createElement('div');
-            carDiv.className = 'car';
-            carDiv.innerHTML = `
-                <img src="${car.image}" alt="${car.title}">
-                <h3>${car.title}</h3>
-                <p>${car.description}</p>
-            `;
-            carListings.appendChild(carDiv);
-        });
+        allCars = data;
+        displayCars(allCars);
     })
     .catch(error => console.error('Error:', error));
+
+    // Search functionality
+    document.querySelector('.search-form').addEventListener('submit', function (e) {
+        e.preventDefault();
+        const searchTerm = document.getElementById('carInput').value.toLowerCase();
+        const filteredCars = allCars.filter(car => car.title.toLowerCase().includes(searchTerm));
+        displayCars(filteredCars);
+    });
 });
+
+function displayCars(cars) {
+    const carListings = document.getElementById('car-listings');
+    carListings.innerHTML = ''; // Clear previous listings
+    cars.forEach(car => {
+        const carDiv = document.createElement('div');
+        carDiv.className = 'car';
+        carDiv.innerHTML = `
+            <img src="${car.image}" alt="${car.title}">
+            <h3>${car.title}</h3>
+            <p>${car.description}</p>
+        `;
+        carListings.appendChild(carDiv);
+    });
+}
