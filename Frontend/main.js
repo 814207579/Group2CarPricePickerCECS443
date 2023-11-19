c35375c86f3cd736b3858b29e2a8e52151816ca3
samnanglath01 <112363663+samnanglath01@users.noreply.github.com>
2023-11-18 22:07:43 -0800

    implemendted login and logout

--- Frontend/main.js
+++ Frontend/main.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function displayCars(cars) {
     const carListings = document.getElementById('car-listings');
-    carListings.innerHTML = ''; // Clear previous listings
+    carListings.innerHTML = ''; // Clear
     cars.forEach(car => {
         const carDiv = document.createElement('div');
         carDiv.className = 'car';
@@ -33,3 +33,26 @@ function displayCars(cars) {
         carListings.appendChild(carDiv);
     });
 }
+//login and redirect to homepage
+function updateLoginLogoutButton() {
+    const loginLogoutButton = document.getElementById('loginLogoutButton');
+
+    if (localStorage.getItem('authToken')) {
+        // logged in
+        loginLogoutButton.textContent = 'Logout';
+        loginLogoutButton.href = '#';
+        loginLogoutButton.onclick = function(e) {
+            e.preventDefault();
+            localStorage.removeItem('authToken');
+            updateLoginLogoutButton(); // Update button state after logout
+        };
+    } else {
+        // not logged in
+        loginLogoutButton.textContent = 'Login';
+        loginLogoutButton.href = '/Frontend/component/login.html';
+        loginLogoutButton.onclick = null; // Remove any old attached click
+    }
+}
+
+// Call function when the DOM is full loaded
+document.addEventListener('DOMContentLoaded', updateLoginLogoutButton);
