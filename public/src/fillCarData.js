a77863019f6517b6685415c40f6c6863006a8a65
Andrew Dutton <andrew.dutton1999@gmail.com>
2023-12-07 08:20:29 -0800

    Clean up and Updated Featured Cars
    - Updated the featured cars section to work
    - Added support to fillCarData for featured Cars
    - Removed old register
    - Removed old car images

--- public/src/fillCarData.js
+++ public/src/fillCarData.js
@@ -1,5 +1,6 @@
 function fillCarData() {
     const parentSlider = document.getElementById("topCarSwiper");
+    const featuredSlider = document.getElementById("featuredCars");
 
     // Clear existing content before filling in new data
     parentSlider.innerHTML = '';
@@ -15,20 +16,20 @@ function fillCarData() {
                     carSlide.classList.add('swiper-slide', 'box');
 
                     carSlide.innerHTML = `
-                        <div class='swiper-slide box'>
-                            <img src='${data.cars[i].image}' alt='car'>
-                            <div class='content'>
+                        <img src='${data.cars[i].image}' alt='car'>
+                        <div class='content'>
                             <h2>${data.cars[i].make.replace("_", " ")}</h2>
                             <h3>${data.cars[i].model.replace("_", " ")}</h3>
-                            <div class='price'> <span>price : </span> $ ${data.cars[i].price}</div>
+                            <div class='price'> 
+                                <span>price : </span> $ ${data.cars[i].price}
+                            </div>
                             <p>
-                            <span class='fas fa-circle'></span> used
-                            <span class='fas fa-circle'></span> ${data.cars[i].model_year}
-                            <span class='fas fa-circle'></span> ${data.cars[i].mileage} miles
+                                <span class='fas fa-circle'></span> used
+                                <span class='fas fa-circle'></span> ${data.cars[i].model_year}
+                                <span class='fas fa-circle'></span> ${data.cars[i].mileage} miles
                             </p>
                             <a target="_blank" href='https://www.cars.com/shopping/results/?stock_type=all&makes%5B%5D=${data.cars[i].make}&models%5B%5D=${data.cars[i].make + "-" + data.cars[i].model}&maximum_distance=30&zip=90804' class='btn'>check out</a>
-                            </div>
-                            </div>
+                        </div>
                         `;
 
                     parentSlider.appendChild(carSlide);
@@ -40,6 +41,8 @@ function fillCarData() {
             console.error('Error:', error);
             alert('Error Loading Car Data');
         });
+
+
 
 
 
@@ -69,6 +72,69 @@ function fillCarData() {
             },
         },
     });
+
+    // Clear existing content before filling in new data
+    featuredSlider.innerHTML = '';
+    // Replace with backendAPI when done
+    fetch("/public/data/CarData.json")
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                //Loop through the JSON data and create HTML for each car
+                for(let i = 0; i < 3; i++) {
+                    //pick 3 random cars
+                    let currentRandomCar = data.cars[Math.floor(Math.random() * 20)]
+                    //update slider
+                    const carSlide = document.createElement('div');
+                    carSlide.classList.add('swiper-slide', 'box');
+
+                    carSlide.innerHTML = `
+                        <img src="${currentRandomCar.image}" alt="">
+                        <div class="content">
+                            <h2>${currentRandomCar.make.replace("_", " ")}</h2>
+                            <h3>${currentRandomCar.model.replace("_", " ")}</h3>
+                            <div class="price">$${currentRandomCar.price}</div>
+                            <a target="_blank" href="https://www.cars.com/shopping/results/?stock_type=all&makes%5B%5D=${currentRandomCar.make}&models%5B%5D=${currentRandomCar.make + "-" + currentRandomCar.model}&maximum_distance=30&zip=90804" class="btn">check out</a>
+                        </div>
+                        `;
+
+                    featuredSlider.appendChild(carSlide);
+                }
+            }
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Error Loading Car Data');
+        });
+
+    //update
+    var swiper = new Swiper(".featured-slider", {
+        grabCursor: true,
+        centeredSlides: true,
+        spaceBetween: 20,
+        loop:true,
+        autoplay: {
+            delay: 9500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable:true,
+        },
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        },
+    });
 }
+
 
 fillCarData()
